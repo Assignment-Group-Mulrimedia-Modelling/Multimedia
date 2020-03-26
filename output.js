54f2ed88f91fe1c7dc66771820e49b50f262047f
MouzammilJowa <42215092+MouzammilJowa@users.noreply.github.com>
2020-03-26 21:35:24 +0400

    Update output.js

--- output.js
+++ output.js
@@ -4,7 +4,7 @@
 	//position where DODO will be drawn
 		var a = 0;
 		var b = 270;
-		var scales = 1.5;
+		var scales = 2;
 		
 		var trackLefts = 1;
 		var trackRights = 3;
@@ -24,12 +24,22 @@
 
 		var currentFrames = 0;
 		var lefts = false;
+		var frequency =200;
+		
+		//dodo blinking variables	
+		var blinking = false;
+		var startBlinking;
+		var endBlinking;
+		var blinkTime 
 
 		var char = new Image;
 		char.src = "dodo1.png";
 		
-		var gun = new Image
-		gun.src = "gun.png"
+		var gun = new Image;
+		gun.src = "gunattack.png";
+
+		var bullet = new Image;
+		bullet.src = "Bullet.png";
 
 	//position where HUNTERS will be drawn
 		var x = 1300; 
@@ -50,23 +60,117 @@
 
 		var width = sheetWidth / cols;
 		var height = sheetHeight / rows;
-
-		var x_pos = 50;
+		
+		//initialise gun
+		var gun_w = 488;
+		var gun_h = 415;
+		var x_pos = 150;
 		var y_pos = 300;
-		var gun_w = 2422;
-		var gun_h = 479;
-		var scale_g = 0.1;
+		var scale_g = 0.3;
+		var speedGun =10;
 
+		//initialise bullet
+		var bullet_w = 46;
+		var bullet_h = 46;
+		var bullet_xpos = 295;
+		var bullet_ypos = 383;
+		var scale_bullet =0.3;
+		var space = false;
+		
+		var z =0;
+		var count=0; //to count number of frames
+
+
+		
+		var controller = new Object();
+		var UP_KEY = 38;
+		var DOWN_KEY = 40;
+		var SPACEBAR = 32;
+
+				
+
+		
+		//intitialise hunters
 		var ennemies = new Array(5);
 		var speed = new Array(5);
-		var yy = new Array(180,230,330,430,480);
+		var yy = new Array(180,230,330,430,480); //y-position of  the hunters
 		var scaleIncrease = new Array(0,0.1,0.15,0.2,0.3);
+
+		/*function createSprite(element, x, y, r, w, h ) {
+			var result = new Object();
+			result.element = element;
+			result.x = x;
+			result.y =y;
+			result.r = r;
+			result.w = w;
+			result.h = h;
+			return result;
+		}*/
+
+		function toggleKey(keyCode, isPressed) {
+			if (keyCode == DOWN_KEY) {	
+				controller.down = isPressed;	
+			}
+
+			if (keyCode == UP_KEY) {
+				controller.up = isPressed;
+			}
+
+			if (keyCode == SPACEBAR) {
+				space = true;
+				controller.space = isPressed;
+			}
+			
+		}
+
+		document.onkeydown = function(evt) {
+			toggleKey(evt.keyCode,true);
+		};
+
+		document.onkeyup = function(evt) {
+			if(evt.keyCode == 32){
+				//canShoot = true;
+				toggleKey(evt.keyCode,true);
+			}
+			else {
+				toggleKey(evt.keyCode,false);
+	
+			}
+		};
+
+		//var bullet = createSprite('bullet',30,40,6, 12,12);
+
+
+		
+		
+
+		function handleControls(){
+			if (controller.down) {
+				y_pos +=  speedGun;
+			}
+
+			if (controller.up) {
+				y_pos -= speedGun;
+			}
+
+			if (controller.space) {
+				bullet_xpos = x_pos + 145;
+				bullet_ypos = y_pos + 83;
+				bullet_xpos+=50;
+		
+				
+			}
+
+			
+		}
+
+		
 
 				
 		
 		for (var i =0;i <5;i++){
-			 ennemies[i] = Math.floor((Math.random() * 1400) + 1300);
-			 speed[i] = Math.floor((Math.random() * 7) + 4);
+			 ennemies[i] = Math.floor((Math.random() * 1400) + 1300); // x-position of ennemies
+			 speed[i] = Math.floor((Math.random() * 10) + 4); //speed of ennemies
 			
 		}
 
@@ -108,11 +212,56 @@
 
 	//functions
 
+		function updatePosition(){
+			bullet_xpos +=50;
+		}
+
 		function updateFrame(){
-			ctx.clearRect(x_pos,y_pos,gun_w*scale_g,gun_h*scale_g);
+			
+			console.log(x_pos);
+			ctx.clearRect(x_pos,y_pos,gun_w*scale_g,gun_h*scale_g); //gun
+			ctx.clearRect(bullet_xpos,bullet_ypos,bullet_w*scale_bullet,bullet_h*scale_bullet);	
+			//ctx.clearRect(bullet_pos,y_pos,bullet_h*scale_bullet,bullet_w*scale_bullet);//bullet
 			ctx.clearRect(a,b, widths*scales, heights*scales); //DODO
 			for (i=0;i<5;i++){
-				ctx.clearRect(ennemies[i], yy[i], width*(scale+scaleIncrease[i]), height*(scale + scaleIncrease[i]));
+				ctx.clearRect(ennemies[i], yy[i], width*(scale+scaleIncrease[i]), height*(scale + scaleIncrease[i])); //hunters
+			}
+			handleControls();
+			//updatePosition();
+			
+
+			/*if (controller.up == true) {
+				y_pos -= speedGun;
+				console.log(movement.up);
+			}
+			else if (controller.down) {
+				
+				y_pos +=speedGun;
+			}
+
+			else if(keyPresses.Enter) {
+				//drawImageBullet();
+				/*while (bullet_pos < canWidth) {
+					
+
+				}*/
+				//bullet_pos +=10;
+				
+				 
+				
+
+			//}
+
+			/*function setPosition(sprite){
+				var e = document.getElementById(sprite.element);
+				e.x +=15; 				
+			}*/
+
+			if (y_pos < 0) {
+				y_pos = canHeight;
+			}
+			else if (y_pos > canHeight) {
+				y_pos =0;
 			}
 			/*ctx.clearRect(x,y, width*scale, height*scale);
 			ctx.clearRect(x1,y+50, width*(scale+0.1),height*(scale+0.1));
@@ -156,54 +305,128 @@
 			x4-=xx4;	*/		
 			srcY = trackRight * height;
 
-			/*if (x  < 50 || x1 < 50 || x2  < 50 || x3 < 50 || x4 < 50) {
-				alert("Game Over");
+			
+			
+
+			if (ennemies[0] < 250 && ennemies[0] > 200 || ennemies[1] < 250 && ennemies[1] > 200 || ennemies[2] < 250 && ennemies[2] > 200 || ennemies[3] < 250 && ennemies[3] > 200 || ennemies[4] < 250 && ennemies[4] > 200) {
+				count =0;
+				blinking = true;
 			}
 
 		
 
-			if(x < -10){
-				x = canWidth;
-			}
+			
+			
 
-			if(x1 < -10){
-				x1 = canWidth;
-			}
 
-			if(x2 < -10){
-				x2 = canWidth;
-			}
 
-			if(x3 < -10){
-				x3 = canWidth;
-			}	
+		
 
-			if(x4 < -10){
-				x4 = canWidth;
+			
+
+			/*if(z < 10){
+				for (i=0; i<5;i++) {
+					if (ennemies[i] < 200) {
+						ennemies[i] = canWidth;
+						//console.log(z);
+						//z++;
+
+
+					}
+				}
+
 			}*/
+				
+				
+
+				
+
+			
+				
+				
+				
+
+			
+			
+			
 
 			currentFrame++;
 				
 			
 		}
 
+		
+
+
+
 
 		
 				
 		function drawImage(){
 			updateFrame();
-			ctx.drawImage(gun,0,0,gun_w,gun_h,x_pos,y_pos,gun_w*scale_g, gun_h*scale_g);	
-			ctx.drawImage(char,srcA,srcB,widths,heights,a,b,widths*scales,heights*scales); //DODO
-			for (i=0;i<5;i++){
-				ctx.drawImage(character,srcX,srcY,width,height,ennemies[i],yy[i],width*(scale+scaleIncrease[i]),height*(scale+scaleIncrease[i]));
-				//console.log(yy[i]);
+			
+
+			
+				if (blinking && Math.floor(Date.now()/frequency) % 2  && count < 20 ) {
+					//setPosition(bullet);
+				
+					//ctx.drawImage(char,srcA,srcB,widths,heights,a,b,widths*scales,heights*scales);
+										
+					ctx.drawImage(gun,0,0,gun_w,gun_h,x_pos,y_pos,gun_w*scale_g, gun_h*scale_g);
+					
+					if (space) {
+						ctx.drawImage(bullet,0,0,bullet_w,bullet_h,bullet_xpos,bullet_ypos,bullet_w*scale_bullet,bullet_h*scale_bullet);
+					}
+
+
+
+					for (i=0;i<5;i++){
+						ctx.drawImage(character,srcX,srcY,width,height,ennemies[i],yy[i],width*(scale+scaleIncrease[i]),height*(scale+scaleIncrease[i]));
+						//console.log(yy[i]);
+					}
+					
+					count++;
+					console.log(count);	
+
+				}
+					
+				
+				
+
+			
+			
+			else {
+
+
+				ctx.drawImage(gun,0,0,gun_w,gun_h,x_pos,y_pos,gun_w*scale_g, gun_h*scale_g);
+				
+				if (space) {
+					ctx.drawImage(bullet,0,0,bullet_w,bullet_h,bullet_xpos,bullet_ypos,bullet_w*scale_bullet,bullet_h*scale_bullet);
+				}
+					
+				
+				
+					
+				ctx.drawImage(char,srcA,srcB,widths,heights,a,b,widths*scales,heights*scales); //DODO
+				for (i=0;i<5;i++){
+					ctx.drawImage(character,srcX,srcY,width,height,ennemies[i],yy[i],width*(scale+scaleIncrease[i]),height*(scale+scaleIncrease[i]));
+					//console.log(yy[i]);
+				}
+
 			}
-			/*ctx.drawImage(character,srcX,srcY,width,height,x,y,width*scale,height*scale);
-			ctx.drawImage(character,srcX,srcY,width,height,x1,y+50,width*(scale+0.1),height*(scale+0.1));
-			ctx.drawImage(character,srcX,srcY,width,height,x2,y+100,width*(scale+0.15),height*(scale+0.15));
-			ctx.drawImage(character,srcX,srcY,width,height,x3,y+200,width*(scale+0.2),height*(scale+0.2));
-			ctx.drawImage(character,srcX,srcY,width,height,x4,y+300,width*(scale+0.3),height*(scale+0.3));	*/
-		} 		
+			
+
+		}
+			
+			
+			
+
+		function drawImageBullet(){
+			//updateFrame();
+			
+
+
+		}
 
 		setInterval(function(){
 			drawImage();
